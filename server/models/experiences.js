815236f8f0c06bdb1595c4450cec8e36f744aa9e
Bety Martinez <trism460@gmail.com>
2022-06-02 21:22:40 -0500

    Add initial model for experiences

---
+++ server/models/experiences.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ExperiencesSchema = new Schema({
+    _iExperience: { type: String, required: true },
+    boolStatus: { type: Boolean, required: true},
+    strDescription: { type: String, required: true },
+    strName: { type: String, required: true },
+    strEmail: { type: String, required: true },
+    strPhone: { type: String, required: false },
+    strPhoto: { type: String, required: false }
+});
+
+module.exports = mongoose.model('Experiences', ExperiencesSchema, 'experiences');
