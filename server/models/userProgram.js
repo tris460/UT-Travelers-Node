bcc92856943848bc20caccd77f114e03f28b34f2
Bety Martinez <trism460@gmail.com>
2022-06-02 21:29:45 -0500

    Add initial model for userProgram

---
+++ server/models/userProgram.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserProgramSchema = new Schema({
+    _idRegister: { type: String, required: true },
+    idUser: { type: String, required: true },
+    idProgram: { type: String, required: true },
+    strStatus: { type: String, required: true },
+    dateDate: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('UserProgram', UserProgramSchema, 'userProgram');
