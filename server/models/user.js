b7e37d92e4c07b9c559a10d75590aba379e74e1f
Bety Martinez <trism460@gmail.com>
2022-06-02 21:12:28 -0500

    Add initial model for users

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    idUser: { type: String, required: true },
+    boolStatus: { type: Boolean, required: true},
+    strRol: { type: String, required: true },
+    strName: { type: String, required: true },
+    strLastName: { type: String, required: true },
+    dateBirth: { type: Date, required: true },
+    strPhone: { type: Number, required: true },
+    strEmail: { type: String, required: true },
+    strPassword: { type: String, required: true },
+    strCareer: { type: String, required: false },
+    strPhoto: { type: String, required: false },
+    arrPrograms: { type: Array, required: false }
+});
+
+module.exports = mongoose.model('User', UserSchema, 'user');
