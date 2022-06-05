1a688845d156c7355103a8f83a9a52f96662e198
Bety Martinez <trism460@gmail.com>
2022-06-04 21:40:22 -0500

    Create controller for user

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,66 @@
+const user = require('../models/user');
+const userCtrl = {};
+
+userCtrl.getUsers = async (req, res) => {
+    const users = await user.find();
+    res.json(users);
+};
+
+userCtrl.createUser = async (req,res) => {
+    const newUser = new user({
+        _idUser: req.body._idUser,
+        boolStatus: req.body.boolStatus,
+        strRol: req.body.strRol,
+        strName: req.body.strName,
+        strLastName: req.body.strLastName,
+        dateBirth: req.body.dateBirth,
+        strPhone: req.body.strPhone,
+        strEmail: req.body.strEmail,
+        strPassword: req.body.strPassword,
+        strCareer: req.body.strCareer,
+        strPhoto: req.body.strPhoto,
+        arrPrograms: req.body.arrPrograms
+    });
+    await newUser.save();
+    res.json({
+       'status' : 'User saved'
+    });
+    console.log(req.body);
+};
+
+userCtrl.getUser = async (req,res) => {
+    const u = await user.findById(req.params.id);
+    res.json(u);
+};
+
+userCtrl.editUser = async (req,res) => {
+    const { id } = req.params;
+    const newUser = {
+        _idUser: req.body._idUser,
+        boolStatus: req.body.boolStatus,
+        strRol: req.body.strRol,
+        strName: req.body.strName,
+        strLastName: req.body.strLastName,
+        dateBirth: req.body.dateBirth,
+        strPhone: req.body.strPhone,
+        strEmail: req.body.strEmail,
+        strPassword: req.body.strPassword,
+        strCareer: req.body.strCareer,
+        strPhoto: req.body.strPhoto,
+        arrPrograms: req.body.arrPrograms
+    }
+    //(id, new object, if it doesn't exist create it)
+    await user.findByIdAndUpdate(id, {$set: newUser}, {new:true});
+    res.json({
+        status: 'User updated'
+    });
+};
+
+userCtrl.deleteUser = async (req,res) => {
+    await user.findByIdAndRemove(req.params.id);
+    res.json({
+        status: 'User deleted'
+    });
+};
+
+module.exports = userCtrl;
