4db10aad097b4920e15ba43fefc2f3536d8979fc
Bety Martinez <trism460@gmail.com>
2022-06-22 18:24:25 -0500

    Require user's photo

--- server/models/experiences.js
+++ server/models/experiences.js
@@ -7,7 +7,7 @@ const ExperiencesSchema = new Schema({
     strName: { type: String, required: true },
     strEmail: { type: String, required: true },
     strPhone: { type: String, required: false },
-    strPhoto: { type: String, required: false }
+    strPhoto: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Experiences', ExperiencesSchema, 'experiences');
